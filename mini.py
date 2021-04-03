be8199137d36fe49260d0ce386bad5afd936bfb4
Craig Moynes <craig.moynes@gmail.com>
2021-04-03 22:39:11 +0100

    Improved handlign of dpad when its pressed down
    Strange behavior on holding down when going left and right
    No logging of tracer stuff. Separate lines for now

--- mini.py
+++ mini.py
@@ -1,7 +1,7 @@
 import pygame
 from pygame.locals import *
 import os
-
+import time
 
 os.putenv('SDL_VIDEODRIVER','fbcon')
 pygame.init()
@@ -24,7 +24,7 @@ HAS_IC = 0
 HAS_IL = 0
 LONG_NAME = 0
 TERM_NAME = 0
-
+LOG_LINES = ['','','','']
 #Add x,y co-ordinates together
 def add(first, second):
     x = first[0] + second[0]
@@ -33,11 +33,13 @@ def add(first, second):
 
 
 def main(screen):
+    global LOG_LINES
     global DO_COLOR,HAS_COLOR,HAS_IC,HAS_IL,LONG_NAME,TERM_NAME
 
     screen = curses.initscr()
+    curses.curs_set(0)
     h, w = screen.getmaxyx()
-    player = [h/2,w/2]
+    player = (h/2,w/2)
 
     DO_COLOR = curses.can_change_color()
     HAS_COLOR = curses.has_colors()
@@ -45,6 +47,8 @@ def main(screen):
     HAS_IL = curses.has_il()
     LONG_NAME = curses.longname()
     TERM_NAME = curses.termname()
+    print "Do Color: {}\nHas Color: {}\nHas insert/delete character capabilities: {}\nHas insert/delete line capabilities: {}\nLong Name: {}\nTerminal Name: {}".format(DO_COLOR, HAS_COLOR, HAS_IC, HAS_IL, LONG_NAME, TERM_NAME)
+    print "LOG_LINES: {}".format(LOG_LINES)
 
     #print "Screen: Height: {}, Width: {}".format(h, w)
     #win = curses.newwin(h, w, 0, 0)
@@ -53,6 +57,9 @@ def main(screen):
     screen.refresh()
 
     running = True
+
+    dpad_down = False
+    next_position = (0,0)
     while(running):
         for event in pygame.event.get():
             if event.type == JOYBUTTONUP and event.button == MENU_BUTTON:
@@ -60,30 +67,39 @@ def main(screen):
                 running = False
             if event.type == DPAD:
                 screen.addstr(1,0, "Event Value: {}".format(event.value))
-                if event.value[1] == 1:
-                    screen.addch(player[0],player[1],' ')
-                    player[0] = player[0]-1
-                    screen.addch(player[0],player[1],curses.ACS_DIAMOND)
-                elif event.value[1] == -1:
-                    screen.addch(player[0],player[1],' ')
-                    player[0] = player[0]+1
-                    screen.addch(player[0],player[1],curses.ACS_DIAMOND)
-                if event.value[0] == 1:
-                    screen.addch(player[0],player[1],' ')
-                    player[1] = player[1]+1
-                    screen.addch(player[0],player[1],curses.ACS_DIAMOND)
-                if event.value[0] == -1:
-                    screen.addch(player[0],player[1],' ')
-                    player[1] = player[1]-1
-                    screen.addch(player[0],player[1],curses.ACS_DIAMOND)
+                if event.value == (0,0):
+                    dpad_down = False
+                else:
+                    dpad_down = True
+
+                if event.value[1] == 1: #Up
+                    next_position = (-1,0)
+                elif event.value[1] == -1: #Down
+                    next_position = (1,0)
+                if event.value[0] == 1: #Right
+                    next_position = (0,1)
+                if event.value[0] == -1: #Left
+                    next_position = (0,-1)
+            
 
             screen.addstr(0,0, "Event: {}".format(event))
             screen.refresh()
             #print "Event: {}".format(event)
+            
+        if dpad_down:
+            #clear last position
+            screen.addstr(2,0, "Position: {}".format(player))
+            screen.addch(player[0],player[1],' ')
+            player = add(player, next_position)
+            screen.addstr(3,0, "Position: {}".format(player))
+            #time.sleep(3)
+            #screen.addstr(2,0, "Player: {}".format(player))
+            screen.addch(player[0],player[1],curses.ACS_DIAMOND)
+            screen.refresh()
+    #End While
+    
 
 if __name__ == '__main__':
     curses.wrapper(main)
 
-print "Preferred Encoding {}".format(code)
-print "Do Color: {}\nHas Color: {}\nHas insert/delete character capabilities: {}\nHas insert/delete line capabilities: {}\nLong Name: {}\nTerminal Name: {}".format(DO_COLOR, HAS_COLOR, HAS_IC, HAS_IL, LONG_NAME, TERM_NAME)
 
